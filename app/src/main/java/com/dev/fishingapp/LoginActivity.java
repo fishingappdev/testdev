3ae739befadfc819c32a7321acb7da79c5010644
Swati Singh <swatisd@goldengekko.com>
2016-04-22 00:06:38 +0530

    SignUp, Edit & Profile Pic Code

--- app/src/main/java/com/dev/fishingapp/LoginActivity.java
+++ app/src/main/java/com/dev/fishingapp/LoginActivity.java
@@ -15,11 +15,11 @@ import com.dev.fishingapp.LoaderCallbacks.LoginCallback;
 /**
  * Created by user on 4/19/2016.
  */
-public class LoginActivity extends AbstractActivity implements OnClickListener{
-    private GradientDrawable loginBtnShape,fbBtnShape,watchBtnShape;
-    private Button mLoginBtn,mFbBtn,mWatchNowBtn;
-    private TextView mForgotPasswordView;
-    private EditText mUserNameEdt,mPasswordEdt;
+public class LoginActivity extends AbstractActivity implements OnClickListener {
+    private GradientDrawable loginBtnShape, fbBtnShape, watchBtnShape;
+    private Button mLoginBtn, mFbBtn, mWatchNowBtn;
+    private TextView mForgotPasswordView, msSignup;
+    private EditText mUserNameEdt, mPasswordEdt;
     private LoaderManager loaderManager;
     private String mUsername;
     private String mPassword;
@@ -28,58 +28,66 @@ public class LoginActivity extends AbstractActivity implements OnClickListener{
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        mLoginBtn=(Button)findViewById(R.id.loginBtn);
-        mFbBtn=(Button)findViewById(R.id.fbBtn);
-        mWatchNowBtn=(Button)findViewById(R.id.watchBtn);
-        mForgotPasswordView=(TextView)findViewById(R.id.forgotPwd);
-        mUserNameEdt=(EditText)findViewById(R.id.username);
-        mPasswordEdt=(EditText)findViewById(R.id.password);
+        mLoginBtn = (Button) findViewById(R.id.loginBtn);
+        mFbBtn = (Button) findViewById(R.id.fbBtn);
+        mWatchNowBtn = (Button) findViewById(R.id.watchBtn);
+        mForgotPasswordView = (TextView) findViewById(R.id.forgotPwd);
+        mUserNameEdt = (EditText) findViewById(R.id.username);
+        mPasswordEdt = (EditText) findViewById(R.id.password);
+        msSignup = (TextView) findViewById(R.id.signup);
 
-        loginBtnShape = (GradientDrawable)mLoginBtn.getBackground();
+        loginBtnShape = (GradientDrawable) mLoginBtn.getBackground();
         loginBtnShape.setColor(getResources().getColor(R.color.drawer_bg));
 
-        fbBtnShape = (GradientDrawable)mFbBtn.getBackground();
+        fbBtnShape = (GradientDrawable) mFbBtn.getBackground();
         fbBtnShape.setColor(getResources().getColor(R.color.fb_btn));
 
-        watchBtnShape = (GradientDrawable)mWatchNowBtn.getBackground();
+        watchBtnShape = (GradientDrawable) mWatchNowBtn.getBackground();
         watchBtnShape.setColor(getResources().getColor(R.color.watch_now_btn));
 
         mLoginBtn.setOnClickListener(this);
         mForgotPasswordView.setOnClickListener(this);
+        msSignup.setOnClickListener(this);
 
-        loaderManager=getSupportLoaderManager();
+        loaderManager = getSupportLoaderManager();
     }
 
     @Override
     public void onClick(View v) {
-        switch(v.getId()){
+        switch (v.getId()) {
             case R.id.loginBtn:
-                boolean IsValidate =validateFields();
-                if(IsValidate) {
-                    if(loaderManager.getLoader(R.id.loader_login)== null){
-                        loaderManager.initLoader(R.id.loader_login, null, new LoginCallback(this,true,mUsername,mPassword));
-                } else {
-                    loaderManager.restartLoader(R.id.loader_login, null, new LoginCallback(this,true,mUsername,mPassword));
-                }
+                boolean IsValidate = validateFields();
+                if (IsValidate) {
+                    if (loaderManager.getLoader(R.id.loader_login) == null) {
+                        loaderManager.initLoader(R.id.loader_login, null, new LoginCallback(this, true, mUsername, mPassword));
+                    } else {
+                        loaderManager.restartLoader(R.id.loader_login, null, new LoginCallback(this, true, mUsername, mPassword));
+                    }
                     Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
                     startActivity(intent);
                 }
                 break;
             case R.id.forgotPwd:
-                Intent forgotpwdIntent=new Intent(LoginActivity.this,ForgotPasswordActivity.class);
+                Intent forgotpwdIntent = new Intent(LoginActivity.this, ForgotPasswordActivity.class);
                 startActivity(forgotpwdIntent);
+                break;
+            case R.id.signup:
+                Intent signupIntent = new Intent(LoginActivity.this, SignUpActivity.class);
+                startActivity(signupIntent);
+                finish();
+                break;
         }
     }
 
-    private boolean validateFields(){
-        mUsername=mUserNameEdt.getText().toString();
-       mPassword=mPasswordEdt.getText().toString();
-        if(mUsername.isEmpty()){
+    private boolean validateFields() {
+        mUsername = mUserNameEdt.getText().toString();
+        mPassword = mPasswordEdt.getText().toString();
+        if (mUsername.isEmpty()) {
             mUserNameEdt.setError(getResources().getString(R.string.empty_username));
             return false;
-        }else if(mPassword.isEmpty()){
+        } else if (mPassword.isEmpty()) {
             mPasswordEdt.setError(getResources().getString(R.string.empty_password));
-            return  false;
+            return false;
         }
         return true;
     }
---
+++ app/src/main/java/com/dev/fishingapp/SignUpActivity.java
@@ -0,0 +1,96 @@
+package com.dev.fishingapp;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.drawable.GradientDrawable;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+/**
+ * Created by user on 4/19/2016.
+ */
+public class SignUpActivity extends Activity implements OnClickListener {
+    private GradientDrawable loginBtnShape;
+    private Button mSignUpBtn;
+    private TextView msigninClick;
+    private EditText mUserNameEdt, mEmailIdEdt, mConfirmEmailId, mFirstName, mLastName;
+    private AutoCompleteTextView mCountry;
+    private String[] values;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+        mSignUpBtn = (Button) findViewById(R.id.signUpBtn);
+        msigninClick = (TextView) findViewById(R.id.signin_click);
+        mUserNameEdt = (EditText) findViewById(R.id.username);
+        mEmailIdEdt = (EditText) findViewById(R.id.email_id);
+        mConfirmEmailId = (EditText) findViewById(R.id.confirm_email_id);
+        mFirstName = (EditText) findViewById(R.id.first_name);
+        mLastName = (EditText) findViewById(R.id.last_name);
+        mCountry = (AutoCompleteTextView) findViewById(R.id.country);
+        values = getResources().getStringArray(R.array.countries_array);
+        ArrayAdapter<String> spinnerArrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, values);
+        spinnerArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item); // The drop down view
+        mCountry.setAdapter(spinnerArrayAdapter);
+        loginBtnShape = (GradientDrawable) mSignUpBtn.getBackground();
+        loginBtnShape.setColor(getResources().getColor(R.color.drawer_bg));
+
+        mSignUpBtn.setOnClickListener(this);
+        msigninClick.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.signUpBtn:
+                boolean IsValidate = validateFields();
+                if (IsValidate) {
+                    Intent intent = new Intent(SignUpActivity.this, HomeActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+                break;
+            case R.id.signin_click:
+                Intent signInIntent = new Intent(SignUpActivity.this, LoginActivity.class);
+                startActivity(signInIntent);
+                finish();
+                break;
+        }
+    }
+
+    private boolean validateFields() {
+        String userName = mUserNameEdt.getText().toString();
+        String emailId = mEmailIdEdt.getText().toString();
+        String confirmEmailId = mConfirmEmailId.getText().toString();
+        String firstName = mFirstName.getText().toString();
+        String lastName = mLastName.getText().toString();
+        if (TextUtils.isEmpty(userName)) {
+            mUserNameEdt.setError(getResources().getString(R.string.empty_username));
+            return false;
+        } else if (TextUtils.isEmpty(emailId)) {
+            mEmailIdEdt.setError(getResources().getString(R.string.empty_email_id));
+            return false;
+        } else if (TextUtils.isEmpty(confirmEmailId)) {
+            mConfirmEmailId.setError(getResources().getString(R.string.empty_confirm_email_id));
+            return false;
+        } else if (TextUtils.isEmpty(firstName)) {
+            mFirstName.setError(getResources().getString(R.string.empty_first_name));
+            return false;
+        } else if (TextUtils.isEmpty(lastName)) {
+            mLastName.setError(getResources().getString(R.string.empty_last_name));
+            return false;
+        } else if (emailId.equals(confirmEmailId)) {
+            mLastName.setError(getResources().getString(R.string.email_confirm_doesnot_match));
+            return false;
+        }
+        return true;
+    }
+}
